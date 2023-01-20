01d9cc29b6516cc61568ae388ea5ab8efe7b0158
SandeepMorya <112643602+Sandeep-morya@users.noreply.github.com>
2023-01-20 11:59:22 +0530

    Update Pagination.jsx

--- testing-movie-app/src/components/Pagination.jsx
+++ testing-movie-app/src/components/Pagination.jsx
@@ -1,10 +1,10 @@
 export default function Pagination() {
     return (
-        <div className="pages">
-            {/* 
+        <di{/* 
             it should contain 
-            * 2 button for prev & Next
-            * and between these buttons an heading tag is required to store the current page number
+            * 2 button for Prev & Next
+            * Buttons name should be Prev & Next
+            * and between these buttons page number is required to store the current page number  (don't use H1)
 
             * on 1st page prev button should be disabled
             * on last page next button should be disabled
